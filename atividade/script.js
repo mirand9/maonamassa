021d4198dc523b040d1d70876de0fb8d1d7d1512
Gabriel <gabmirandax@gmail.com>
2026-09-23 10:33:07 -0300

    parte111

--- atividade/script.js
+++ atividade/script.js
@@ -5,24 +5,29 @@ const btnResetar = document.getElementById('Resetar');
 const btnTema = document.getElementById('Alterar-tema'); // Lembre de atualizar o ID no HTML para 'Alterar-tema' se necessário
 const meuBody = document.body;
 
-let tamanhoAtual = 16; 
+let tamanhoAtual = localStorage.getItem("fontSize") || 16;
 
-
+addEventListener('DOMContentLoaded', function() {
+    meuBody.style.fontSize = tamanhoAtual + 'px';
+});
 
 btnAumentar.addEventListener('click', function() {
     tamanhoAtual = tamanhoAtual +2;
     meuBody.style.fontSize = tamanhoAtual + 'px';
+    localStorage.setItem("fontSize", tamanhoAtual);
     
 });
 
 btnDiminuir.addEventListener('click', function() {
     tamanhoAtual = tamanhoAtual -2;
     meuBody.style.fontSize = tamanhoAtual + 'px';
+    localStorage.setItem("fontSize", tamanhoAtual);
 });
 
 btnResetar.addEventListener('click', function() {
     tamanhoAtual = 16;
     meuBody.style.fontSize = tamanhoAtual + 'px';
+    localStorage.setItem("fontSize", tamanhoAtual);
    
 });   
 
@@ -30,4 +35,5 @@ btnResetar.addEventListener('click', function() {
 btnTema.addEventListener('click', function() {
     meuBody.classList.toggle('dark-mode');
 });
+
 
